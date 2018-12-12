030d3e262d264e3e1b02c0655dabee70697d2d9d
Wayne Collier <ke4tri@gmail.com>
2018-12-12 13:50:37 -0600

    listingForm is printing

--- src/App/App.js
+++ src/App/App.js
@@ -5,7 +5,7 @@ import connection from '../helpers/data/connection';
 import Auth from '../components/Auth/Auth';
 import Listings from '../components/Listings/Listings';
 import Buildings from '../components/Buildings/buildings';
-// import ListingForm from '../components/ListingForm/listingform';
+import ListingForm from '../components/ListingForm/listingForm';
 import MyNavbar from '../components/MyNavbar/myNavbar';
 import './App.scss';
 import authRequests from '../helpers/data/authRequests';
@@ -59,6 +59,7 @@ class App extends Component {
         <MyNavbar isAuthed={this.state.authed} logoutClickEvent={logoutClickEvent} />
         <Listings />
         <Buildings />
+        <ListingForm />
         {/* <Buildings />
         <ListingForm /> */}
       </div>
