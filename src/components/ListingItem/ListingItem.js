282cb567d9a51e415c0f7314791da3c6540a3075
Wayne Collier <ke4tri@gmail.com>
2018-12-15 12:04:13 -0600

    delete button printing

--- src/components/ListingItem/ListingItem.js
+++ src/components/ListingItem/ListingItem.js
@@ -2,19 +2,36 @@ import React from 'react';
 import listingShape from '../../helpers/propz/listingShape';
 import formatPrice from '../../helpers/formatPrice';
 import './ListingItem.scss';
+import authRequests from '../../helpers/data/authRequests';
 
 class ListingItem extends React.Component {
   static propTypes = {
     listing: listingShape,
   }
 
+  // listing below has all the .informationyou need
   render() {
     const { listing } = this.props;
+    const uid = authRequests.getCurrentUid();
+
+    const makeButtons = () => {
+      if (listing.uid === uid) {
+        return (
+          <div>
+            <span className="col">
+            <button className="btn btn-default">
+            <i className="fas fa-trash-alt"></i>
+            </button></span>
+          </div>
+        );
+      }
+      return <span className="col-2"></span>;
+    };
     return (
       <li className="listing-item text-center">
         <span className="col-7">{listing.address}</span>
         <span className="col-3">{formatPrice(listing.price)}</span>
-
+        {makeButtons()}
       </li>
     );
   }
