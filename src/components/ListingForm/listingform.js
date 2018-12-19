14ece0d5b9fec4d995f9dd7e9e2f7cb1635ef739
Wayne Collier <ke4tri@gmail.com>
2018-12-18 19:18:40 -0600

    form

--- src/components/ListingForm/listingform.js
+++ src/components/ListingForm/listingform.js
@@ -8,7 +8,19 @@ class listingForm extends React.Component {
   render() {
     return (
       <div className="listingForm col">
-       <h2>Lisgin Form</h2>
+       <h2>Add New Listing:</h2>
+       <form>
+          <div className="form-group">
+            <label for="address">Address :</label>
+            <input 
+            type="text" 
+            className="form-control" 
+            id="address" 
+            aria-describedby="emailHelp" 
+            placeholder="123 Main Street Nashville, Tn 37209" />
+          </div>
+          <button className="btn btn-danger">Save Listing</button>
+        </form>
       </div>
     );
   }
