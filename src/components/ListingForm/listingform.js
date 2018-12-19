14c8e219532f86f53b14eb714409b52062b10350
Wayne Collier <ke4tri@gmail.com>
2018-12-18 19:48:16 -0600

    address logging state update

--- src/components/ListingForm/listingform.js
+++ src/components/ListingForm/listingform.js
@@ -4,20 +4,55 @@ import {
   Col, Row, Button, Form, FormGroup, Label, Input, FormText,
 } from 'reactstrap';
 
+const defaultListing = {
+  address: '',
+  squareFootage: 0,
+  price: 0,
+  numBeds: 0,
+  numBaths: 0,
+  heating: '',
+  cooling: '',
+  imageUrl: '',
+  uid: '',
+};
+
 class listingForm extends React.Component {
+  state = {
+    newListing: defaultListing
+  }
+
+formFieldStringState = (name, e) => {
+  e.preventDefault();
+  const tempListing = { ...this.state.newListing }; // spread operator
+  tempListing[name] = e.target.value;
+  this.setState({ newListing: tempListing });
+}
+
+// fromFieldStringState = (name, e) => {
+//   const tempListing = { ...this.state.newListing }; // spread operator
+//   tempListing[name] = e.target.value * 1;
+//   this.setState({ newListing: tempListing });
+// }
+
+  addressChange = e => this.formFieldStringState('address', e);
+
   render() {
+    const { newListing } = this.state;
     return (
       <div className="listingForm col">
        <h2>Add New Listing:</h2>
        <form>
           <div className="form-group">
-            <label for="address">Address :</label>
+            <label htmlFor="address">Address :</label>
             <input 
             type="text" 
             className="form-control" 
             id="address" 
             aria-describedby="emailHelp" 
-            placeholder="123 Main Street Nashville, Tn 37209" />
+            placeholder="123 Main Street Nashville, Tn 37209" 
+            value={newListing.address}
+            onChange={this.addressChange}
+            />
           </div>
           <button className="btn btn-danger">Save Listing</button>
         </form>
